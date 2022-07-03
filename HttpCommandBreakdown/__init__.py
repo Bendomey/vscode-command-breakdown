e7208116f09014420dc4f6b8330dbfe22a7366a9
Domey Benjamin <domeybenjamin1@gmail.com>
2022-07-03 21:43:22 +0000

    added response

--- HttpCommandBreakdown/__init__.py
+++ HttpCommandBreakdown/__init__.py
@@ -1,24 +1,34 @@
+from email import header
 import logging
 
 import azure.functions as func
+import json
 
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
+    query = req.params.get('query')
+    if not query:
         try:
             req_body = req.get_json()
         except ValueError:
             pass
         else:
-            name = req_body.get('name')
+            query = req_body.get('query')
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    if query:
+        return func.HttpResponse(json.dumps([{
+            "command":"copy",
+            "line":10
+        },{
+            "command":"paste",
+            "line":52
+        }]) ,
+             status_code=200
+        )
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
-             status_code=200
+             json.dumps({"error":"Please provide a query"}) ,
+             status_code=400
         )
